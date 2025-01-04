9a978e53addf93ab75917674a66f52e0b1e6e105
Erick <erickbarretodasilva@gmail.com>
2025-01-04 18:35:08 -0300

    testando as rotas

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,4 @@
 import pytest
-
-# from fastapi import FastAPI
-
-# from fastapi.testclient import TestClient
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -16,3 +12,35 @@ def test_home():
     assert response.json() == {
         "mensagem": "Bem-vindo à API de Recomendação de Produtos"
     }
+
+
+def test_criar_produtos():
+    response = client.post(
+        "/produtos/",
+        json={
+            "nome": "Produtos A",
+            "categoria": "Categoria 1",
+            "tags": ["tag1", "tag2"],
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["nome"] == "Produtos A"
+
+
+def test_listar_produtos():
+    response = client.get("/produtos/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_criar_usuarios():
+    response = client.post("/usuarios/", params={"nome": "Usuário Teste"})
+    usuario_data = response.json()
+    assert usuario_data["id"] == 1
+    assert usuario_data["nome"] == "Usuário Teste"
+
+
+def test_listar_usuarios():
+    response = client.get("/usuarios/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
