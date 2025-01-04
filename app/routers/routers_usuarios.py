31bb5139502b20f4f94c7ac7ef37ca068fd81589
Erick <erickbarretodasilva@gmail.com>
2025-01-03 23:41:06 -0300

    aplicando o black formatter

--- app/routers/routers_usuarios.py
+++ app/routers/routers_usuarios.py
@@ -13,6 +13,13 @@ contador_usuario: int = 1
 
 @router.post("/usuarios/", response_model=Usuario)
 def criar_usuario(nome: str) -> Usuario:
+    """
+    Cria um novo usuário.
+    Args:
+        nome (str): O nome do usuário a ser criado.
+    Returns:
+        Usuario: O objeto do usuário recém-criado com um ID gerado.
+    """
     global ContadorUsuario
     novo_usuario = Usuario(id=ContadorUsuario, nome=nome)
     Usuarios.append(novo_usuario)
@@ -25,4 +32,9 @@ def criar_usuario(nome: str) -> Usuario:
 
 @router.get("/usuarios/", response_model=List[Usuario])
 def listar_usuarios() -> List[Usuario]:
+    """
+    Lista todos os usuários cadastrados.
+    Returns:
+        List[Usuario]: Uma lista de objetos de usuários cadastrados.
+    """
     return Usuarios
